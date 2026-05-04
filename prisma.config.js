a22639749ff43544be27b94091c1a609a24d95ed
Gustavo Lisboa <155676204+lisboathecoder@users.noreply.github.com>
2026-05-04 11:13:39 -0300

    Revert "arruma crud simulado"

--- prisma.config.js
+++ prisma.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         path: 'prisma/migrations',
         seed: 'node prisma/seed.js',
     },
-datasource: {
-    url: process.env['DIRECT_URL'],
-},
+    datasource: {
+        url: process.env['DATABASE_URL'],
+    },
 });
