10dc7ae2a78cf35e539ba359807d5ab2f1e200eb
Arthur Morais <mlkliso4000@gmail.com>
2026-05-11 13:47:51 -0300

    feat: modifica a lógica de autenticação de rotas da api para evitar invasores malvados e previnir nossa api -> mais segurança = mais empenho! Viva The Rats JS

--- src/routes/participantesRoute.js
+++ src/routes/participantesRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as controller from '../controllers/participantesController.js';
+import { authAdmin, authPublic } from '../utils/apiKey.js';
 
 const router = express.Router();
 
-router.post('/', controller.criar);
-router.get('/', controller.buscarTodos);
-router.get('/:id', controller.buscarPorId);
-router.put('/:id', controller.atualizar);
-router.delete('/:id', controller.deletar);
+router.post('/', authAdmin, controller.criar);
+router.get('/', authPublic, controller.buscarTodos);
+router.get('/:id', authPublic, controller.buscarPorId);
+router.put('/:id', authAdmin, controller.atualizar);
+router.delete('/:id', authAdmin, controller.deletar);
 
 export default router;
